a027b5c8db9b39d68220abe12dd6cffd9a5aff34
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-05-19 23:36:33 +0430

    Use new APIs

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,8 +1,11 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+/* LOCAL IMPORTS */
 import AuthAPI from "./api/AuthAPI";
-import UserAPI from "./api/UserAPI";
-import ProductsAPI from "./api/ProductsAPI";
+import CartAPI from "./api/CartAPI";
 import CategoriesAPI from "./api/CategoriesAPI";
+import OrdersAPI from "./api/OrdersAPI";
+import PostalCodesAPI from "./api/PostalCodesAPI";
+import ProductsAPI from "./api/ProductsAPI";
 import TimingAPI from "./api/TimingAPI";
 
 export const GlobalState = createContext();
@@ -13,13 +16,21 @@ export const DataProvider = ({ children }) => {
   const productsAPI = ProductsAPI();
   const categoriesAPI = CategoriesAPI();
   const timingsAPI = TimingAPI();
+  const postalCodesAPI = PostalCodesAPI();
+  const cartAPI = CartAPI();
+  const ordersAPI = OrdersAPI(authAPI.token[0], authAPI.isAdmin[0]);
+  const fromTo = useState("");
 
   const state = {
     authAPI,
     productsAPI,
     userAPI,
     categoriesAPI,
-    timingsAPI
+    timingsAPI,
+    postalCodesAPI,
+    ordersAPI,
+    cartAPI,
+    fromTo,
   };
 
   return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
